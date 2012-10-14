9dd4cad80b618ae57f4423c22738e8fa3c61e9b9
akabeko <akabekobeko@gmail.com>
2012-10-14 14:38:43 +0900

    Implemented: Support the full screen player.
    refs #59

--- VideoPlayer/VideoPlayer/AVPlayerViewController.m
+++ VideoPlayer/VideoPlayer/AVPlayerViewController.m
@@ -123,6 +123,36 @@ static void* AVPlayerViewControllerStatusObservationContext = &AVPlayerViewContr
 }
 
 /**
+ * 画面が表示される時に発生します。
+ *
+ * @param animated 表示アニメーションを有効にする場合は YES。それ以外は NO。
+ */
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+
+    [self setWantsFullScreenLayout:YES];
+    [[UIApplication sharedApplication] setStatusBarStyle:UIStatusBarStyleBlackTranslucent];
+    [self.navigationController.navigationBar setBarStyle:UIBarStyleBlackTranslucent];
+    [self.navigationController.navigationBar setTranslucent:YES];
+    [self.navigationController.view setNeedsLayout];
+}
+
+/**
+ * 他の画面へ切り替わる時に発生します。
+ *
+ * @param animated 表示アニメーションを有効にする場合は YES。それ以外は NO。
+ */
+- (void)viewWillDisappear:(BOOL)animated
+{
+    [[UIApplication sharedApplication] setStatusBarStyle:UIStatusBarStyleBlackOpaque];
+    [self.navigationController.navigationBar setBarStyle:UIBarStyleBlackOpaque];
+    [self.navigationController.navigationBar setTranslucent:NO];
+    
+    [super viewWillDisappear:animated];
+}
+
+/**
  * プロパティが更新された時に発生します。
  *
  * @param keyPath 変更されたプロパティ。
--- VideoPlayer/VideoPlayer/MoviePlayerViewController.m
+++ VideoPlayer/VideoPlayer/MoviePlayerViewController.m
@@ -84,4 +84,34 @@
     [super viewDidUnload];
 }
 
+/**
+ * 画面が表示される時に発生します。
+ *
+ * @param animated 表示アニメーションを有効にする場合は YES。それ以外は NO。
+ */
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    
+    [self setWantsFullScreenLayout:YES];
+    [[UIApplication sharedApplication] setStatusBarStyle:UIStatusBarStyleBlackTranslucent];
+    [self.navigationController.navigationBar setBarStyle:UIBarStyleBlackTranslucent];
+    [self.navigationController.navigationBar setTranslucent:YES];
+    [self.navigationController.view setNeedsLayout];
+}
+
+/**
+ * 他の画面へ切り替わる時に発生します。
+ *
+ * @param animated 表示アニメーションを有効にする場合は YES。それ以外は NO。
+ */
+- (void)viewWillDisappear:(BOOL)animated
+{
+    [[UIApplication sharedApplication] setStatusBarStyle:UIStatusBarStyleBlackOpaque];
+    [self.navigationController.navigationBar setBarStyle:UIBarStyleBlackOpaque];
+    [self.navigationController.navigationBar setTranslucent:NO];
+    
+    [super viewWillDisappear:animated];
+}
+
 @end
