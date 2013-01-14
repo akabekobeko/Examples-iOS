47ae7f0d2bab818f7bea5c3dc23429a32d2a3721
akabeko <akabekobeko@gmail.com>
2013-01-14 16:37:26 +0900

    不要なファイルを削除

---
+++ InAppPurchase/InAppPurchase/InAppPurchaseManager.h
@@ -0,0 +1,13 @@
+//
+//  InAppPurchaseManager.h
+//  InAppPurchase
+//
+//  Created by Akabeko on 2012/11/10.
+//  Copyright (c) 2012年 Akabeko. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface InAppPurchaseManager : NSObject
+
+@end
---
+++ InAppPurchase/InAppPurchase/InAppPurchaseManager.m
@@ -0,0 +1,13 @@
+//
+//  InAppPurchaseManager.m
+//  InAppPurchase
+//
+//  Created by Akabeko on 2012/11/10.
+//  Copyright (c) 2012年 Akabeko. All rights reserved.
+//
+
+#import "InAppPurchaseManager.h"
+
+@implementation InAppPurchaseManager
+
+@end
